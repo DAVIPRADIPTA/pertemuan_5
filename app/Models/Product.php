3633f727c5754aa7173e55d739e1c62da22115d1
DAVIPRADIPTA <davipradipta456@gmail.com>
2025-12-04 18:25:38 +0700

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+    'category_id', 'name', 'slug', 'image', 'description', 'price', 'stock'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
